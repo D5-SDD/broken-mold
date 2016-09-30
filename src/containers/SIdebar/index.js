6289b1f31dd323613c275639ad463180a52b765e
Will <will@wgrh.io>
2016-09-29 22:13:08 -0400

    Trying to implement some sort of sidebar

---
+++ src/containers/SIdebar/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+import React from 'react';
+
+class AppContainer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="sidebar">
+      </div>
+    );
+  }
+}
+
+export default AppContainer;
