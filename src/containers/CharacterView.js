93668e18598235548b4b1852352278c385747a55
Will Rigby-Hall <will@wgrh.io>
2016-10-15 23:46:15 -0400

    CharacterView switches to CharacterSheet container when a character is selected

--- src/containers/CharacterView.js
+++ src/containers/CharacterView.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import CharacterMenu from '../components/CharacterMenu';
+import CharacterSheet from './CharacterSheet';
 
 import '../stylesheets/containers/CharacterView';
 
@@ -10,15 +11,14 @@ class CharacterView extends React.Component {
     super(props);
 
     this.state = {
-      viewState: 0 // 0: character menu
+      viewState: 0, // 0: character menu, 1: character sheet
+      currentCharacter: null
     };
 
     this.selectCharacterCB = this.selectCharacterCB.bind(this);
   }
 
   selectCharacterCB(node) {
-    console.log('selectCharacterCB');
-
     var char;
     for (let i = 0; i < this.props.characterMap.length; i++) {
       if (this.props.characterMap[i].label === node.label) {
@@ -26,16 +26,29 @@ class CharacterView extends React.Component {
         break;
       }
     }
-    console.log(char);
+
+    this.setState({viewState: 1, currentCharacter: char});
   }
 
   render() {
-    return (
-      <div className="character-view">
+    var CV;
+    if (this.state.viewState === 0) {
+      CV = (
         <CharacterMenu
           characterMap={this.props.characterMap}
           selectCharacterCB={this.selectCharacterCB}
         />
+      );
+    } else if (this.state.viewState === 1) {
+      CV = (
+        <CharacterSheet
+          currentCharacter={this.state.currentCharacter}
+        />
+      );
+    }
+    return (
+      <div className="character-view">
+        {CV}
       </div>
     );
   }
