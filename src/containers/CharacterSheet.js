2686160b191ec24aa7129e6fbf39df5e926736e2
Will Rigby-Hall <will@wgrh.io>
2016-10-19 15:17:05 -0400

    Figuring out the grid system

--- src/containers/CharacterSheet.js
+++ src/containers/CharacterSheet.js
@@ -2,25 +2,46 @@
 
 import React from 'react';
 import {Grid, Row, Col} from 'react-bootstrap';
-import CharacterSheetSidebar from '../components/CharacterSheetSidebar';
+// CharacterSheetSidebar from '../components/CharacterSheetSidebar';
 
 import '../stylesheets/containers/CharacterSheet';
 
 class CharacterSheet extends React.Component {
   constructor(props) {
     super(props);
+
+    var viewState = 0;
+    if (this.props.character === null) {
+      viewState = 1;
+    }
+
+    this.state = {
+      viewState: viewState
+    };
   }
 
   render() {
     var CS_GRID = null;
-    // NOTE: If this gets called with this.props.character === null, we are
-    // creating a new character
+    if (this.state.viewState === 0) {
+      // TODO: Character View
+      CS_GRID = (
+        <Grid className="character-sheet-grid">
+          <Row>
+            <Col className="col" md={8}>
+              <code>&lt;{'Col xs={12} md={8}'} /&gt;</code>
+            </Col>
+            <Col className="col" md={4}>
+              <code>&lt;{'Col xs={6} md={4}'} /&gt;</code>
+            </Col>
+          </Row>
+        </Grid>
+      );
+    } else {
+      // TODO: Character creation
+    }
+
     return (
       <div className="character-sheet">
-        <CharacterSheetSidebar
-          character={this.props.character}
-          exitCharacterSheetCB={this.props.exitCharacterSheetCB}
-        />
         {CS_GRID}
       </div>
     );
