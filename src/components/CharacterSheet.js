6372eef6375f7d1987e7ee31e9af8af6b5333b92
Will Rigby-Hall <will@wgrh.io>
2016-10-25 16:03:58 -0400

    Adding functionality to charactersheets

--- src/components/CharacterSheet.js
+++ src/components/CharacterSheet.js
@@ -3,21 +3,81 @@
 import React from 'react';
 import {Row, Col, Panel} from 'react-bootstrap';
 
+import {SKILLS} from '../../lib/Character';
+
+const ICONS = {
+  savingThrows: {
+    true: 'star',
+    false: 'star-o'
+  }
+};
+
+function getIcon(name, data) {
+  if (data !== undefined) {
+    return (<i className={'fa fa-' + ICONS[name][data]} aria-hidden="true"/>);
+  } else if (name !== undefined) {
+    return (<i className={'fa fa-' + ICONS[name]} aria-hidden="true"/>);
+  } else {
+    return (<i className="fa fa-question" aria-hidden="true"/>);
+  }
+}
+
+export class AbilityScores extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    return (
+      <div className="ability-scores">
+        <AbilityScore
+          name="strength"
+          savingThrows={this.props.savingThrows.strength}
+          skills={this.props.skills}
+          value={this.props.abilityScores.strength}
+        />
+      </div>
+    );
+  }
+}
+
+AbilityScores.propTypes = {
+  abilityScores: React.PropTypes.object.isRequired,
+  savingThrows: React.PropTypes.object.isRequired,
+  skills: React.PropTypes.object.isRequired
+};
+
 export class AbilityScore extends React.Component {
 	constructor(props) {
 		super(props);
-
-    console.log(this.props.scoreData);
 	}
 
 	render() {
     var name = this.props.name;
-    var className = 'ability-score ' + name;
+    var savingThrowsIcon = getIcon('savingThrows', this.props.savingThrows.proficient);
+    var skills = [];
+    for (let i = 0; i < SKILLS[name].length; i++) {
+      let skill = SKILLS[name][i];
+      skills[skill] = this.props.skills[skill];
+    }
+
 		return (
-      <Row>
+      <Row className="ability-score" id={name} >
+        <Col className="col" md={4}>
+          <Panel header={name} className="centered">
+            {this.props.value}
+          </Panel>
+        </Col>
         <Col className="col" md={8}>
-          <Panel header={name}>{this.props.scoreData.value}</Panel>
-          <Panel header={name}>{this.props.scoreData.value}</Panel>
+          <Panel>
+            <span className="saving-throws">
+              {savingThrowsIcon} {this.props.savingThrows.value} Saving Throws
+            </span>
+            <div className="skills">
+              {skills}
+            </div>
+          </Panel>
         </Col>
       </Row>
 		);
@@ -26,7 +86,9 @@ export class AbilityScore extends React.Component {
 
 AbilityScore.propTypes = {
 	name: React.PropTypes.string.isRequired,
-  scoreData: React.PropTypes.object.isRequired
+  savingThrows: React.PropTypes.object.isRequired,
+  skills: React.PropTypes.object.isRequired,
+  value: React.PropTypes.number.isRequired
 };
 
 export class Header extends React.Component {
--- src/containers/CharacterSheet.js
+++ src/containers/CharacterSheet.js
@@ -1,8 +1,8 @@
 'use strict';
 
 import React from 'react';
-import {Grid, Row, Col, Panel} from 'react-bootstrap';
-import {AbilityScore} from '../components/CharacterSheet';
+import {Grid, Row, Col} from 'react-bootstrap';
+import {AbilityScores} from '../components/CharacterSheet';
 
 import '../stylesheets/containers/CharacterSheet';
 
@@ -22,19 +22,19 @@ class CharacterSheet extends React.Component {
     console.log(this.props.character);
   }
   render() {
+    var character = this.props.character;
     var CS_GRID = null;
     if (this.state.viewState === 0) {
       // TODO: Character View
-      var strengthData = {};
-      strengthData.value = this.props.character.abilityScores.strength;
-      strengthData.savingThrows = this.props.character.savingThrows.strength;
       CS_GRID = (
         <Grid className="character-sheet-grid">
           <Row>
             <Col className="col" md={4}>
-              <Panel header="Proficiency Bonus">{this.props.character.proficiencyBonus}</Panel>
-              <Panel header="Inspiration">{this.props.character.inspiration}</Panel>
-              <AbilityScore name="Strength" scoreData={strengthData} />
+              <AbilityScores
+                abilityScores={character.abilityScores}
+                savingThrows={character.savingThrows}
+                skills={character.skills}
+              />
             </Col>
             <Col className="col" md={4}>
               <p>Health and Attacks</p>
