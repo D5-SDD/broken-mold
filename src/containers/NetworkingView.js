093262898730055ec3d554304663328febec0a07
Will Rigby-Hall <will@wgrh.io>
2016-11-14 18:10:44 -0500

    forgot to add a file

---
+++ src/containers/NetworkingView.js
@@ -0,0 +1,54 @@
+'use strict';
+
+// Import libraries
+import React from 'react';
+import Button from 'react-bootstrap/lib/Button';
+
+// Import internal libraries
+import {startUDPBroadcast} from '../../lib/Networking';
+
+// MIKE! THIS IS A TUTORIAL FOR YOU!
+// ANY FUNCTIONS THAT YOU NEED TO CALL/TEST YOU IMPORT
+// IN THE CURLY BRACES ABOVE THIS COMMENT.
+// TO ASSIGN THAT FUNCTION TO BE CALLED BY A BUTTON,
+// CHANGE THE ONCLICK PROPERTY OF THAT BUTTON TO CALL THAT FUNCTION
+
+class NetworkingView extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+      <div className="networking-view">
+        <nav className="navigation" id="header">
+          <Button
+            bsStyle="primary"
+            bsSize="small"
+            onClick={startUDPBroadcast}
+          >
+            Button 1
+          </Button>
+          <Button
+            bsStyle="primary"
+            bsSize="small"
+            onClick={startUDPBroadcast}
+          >
+            Button 2
+          </Button>
+          <Button
+            bsStyle="primary"
+            bsSize="small"
+            onClick={startUDPBroadcast}
+          >
+            Button 3
+          </Button>
+        </nav>
+      </div>
+		);
+	}
+}
+
+NetworkingView.propTypes = {};
+
+export default NetworkingView;
