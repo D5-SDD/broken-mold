9b33641ad77a2481dadc2e6ee5fd745257c67399
Will Rigby-Hall <will@wgrh.io>
2016-11-17 18:43:58 -0500

    Fixed DM tabs, not proud but functional

--- src/containers/DMView.js
+++ src/containers/DMView.js
@@ -2,10 +2,10 @@
 
 // Import libraries
 import React from 'react';
-import {Button} from 'react-bootstrap';
+import {Button, Tab, Tabs} from 'react-bootstrap';
 
 // Import containers
-import DMTabs from './DMTabs';
+import CharacterSheet from './CharacterSheet';
 
 // Import internal libraries
 import Character from '../../lib/Character';
@@ -21,20 +21,41 @@ class DMView extends React.Component {
 
     this.characters = [];
 
+    this.characterReceivedCB = this.characterReceivedCB.bind(this);
     // TODO: remove character from characters upon disconnect
+  }
+
+  characterReceivedCB(charLocation) {
+    this.characters.push(new Character(charLocation));
+    this.forceUpdate();
   }
 
   openConnectionCB() {
     startUDPBroadcast(true);
     startTCPServer((charLocation) => {
-      var test = new Character(charLocation);
-      this.characters.push(test);
-      console.log(this.characters);
-      //this.forceUpdate();
+      this.characterReceivedCB(charLocation);
     }, []);
   }
 
   render() {
+    var tabContainer = null;
+    if (this.characters.length > 0) {
+      var tabs = [];
+      for (let i = 0; i < this.characters.length; i++) {
+        tabs.push(
+          <Tab key={i} eventKey={i} title={this.characters[i].name}>
+            <CharacterSheet character={this.characters[i]} />
+          </Tab>
+        );
+      }
+
+      tabContainer = (
+        <Tabs animation={false} id="dm-tabs" className="dm-tabs">
+          {tabs}
+        </Tabs>
+      );
+    }
+
     return (
       <div className="dm-view">
         <nav className="navigation" id="header">
@@ -75,7 +96,7 @@ class DMView extends React.Component {
             UDP Listen
           </Button>
         </nav>
-        <DMTabs characters={this.characters} />
+        {tabContainer}
       </div>
     );
   }
